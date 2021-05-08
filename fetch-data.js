251864dbeb66d2fd1ff0bed18946afd3a2652916
davidcubesvk <davidcubesvkdev@gmail.com>
2021-05-08 11:50:14 +0200

    Comments and optimizations.

--- fetch-data.js
+++ fetch-data.js
@@ -1,27 +1,44 @@
+// HTTPS module
 const https = require("https");
-const fs = require("fs");
-const { json } = require("express");
 
-let pluginDownloads = 0, projects = 6, averageStars = 0;
-const downloadURL = new Map();
+// Variables for API data fetch
+// TODO: GitHub repository sync
+let pluginDownloads = 0, projects = 32, averageStars = 0;
+// Download URLs
+let downloadURL = new Map();
+// Page URLs
+let pageURL = new Map();
+
+// Module export functions
 module.exports = {
+
+    // Returns amount of plugin downloads
     getPluginDownloads: function() {
         return pluginDownloads;
     },
 
+    // Returns the amount of projects
     getProjects: function() {
         return projects;
     },
 
+    // Returns average stars in reviews
     getAverageStars: function() {
         return averageStars;
     },
 
+    // Returns download URL of plugin specified by the ID
     getDownloadURL: function(pluginId) {
         return downloadURL.get(pluginId);
+    },
+
+    // Returns project page URL of plugin specified by the ID
+    getPageURL: function(pluginId) {
+        return pageURL.get(pluginId);
     }
 };
 
+// Request options for Spiget API
 const requestOptions = {
     host: "api.spiget.org",
     port: 443,
@@ -29,37 +46,58 @@ const requestOptions = {
     method: "GET"
 }
 
+// Handles response data
 function handleResponse(response) {
+    // Encoding
     response.setEncoding("utf8");
+    // Data chunks
     let data = "";
+    // On data receive
     response.on("data", function(chunk) {
         data += chunk;
     });
+    // On response data chunk read end
     response.on("end", function() {
         processData(data);
     });
 }
+
+// Handles errors
 function handleError(error) {
     console.log(error);
 }
+
+// Processes finalized data
 function processData(data) {
+    // Reset
     averageStars = 0;
     pluginDownloads = 0;
+    // JSON object
     const json = JSON.parse(data);
+    // For each plugin
     json.forEach(element => {
+        // Add downloads and average stars
         pluginDownloads += element.downloads;
         averageStars += element.rating.average;
-        downloadURL.set(element.id, "https://spigotmc.org/" + element.url);
+        // Set the download and page URL
+        downloadURL.set(element.id, "https://spigotmc.org/" + element.file.url);
+        pageURL.set(element.id, "https://spigotmc.org/resources/" + element.id);
     });
+    // Calculate and round average stars
     averageStars = Math.round(averageStars / json.length);
 }
+
+// Requests data from Spiget API
 function request() {
-    console.log("request")
+    // Create request
     let request = https.request(requestOptions, handleResponse);
+    // Add handler
     request.on("error", handleError);
+    // End
     request.end();
 }
-console.log("run")
+
+// Run the request
 request();
+// Run every 10 minutes
 setInterval(request, 600000);
-console.log("end")
--- index.js
+++ index.js
@@ -1,20 +1,45 @@
 // Express
 const express = require("express");
-const { getPluginDownloads, getProjects, getAverageStars, getDownloadURL } = require("./fetch-data");
+// Fetch data module
 const data = require("./fetch-data");
 // The app
 const app = express()
 // Port
 const port = 8080
 
+// Set views directory
 app.set('views', './views')
+// Set view engine
 app.set('view engine', 'pug')
+// Set static file directory
 app.use(express.static('public'))
 
+// Handle home requests
 app.get("/", (req, res) => {
-    res.render("home", {plugin_downloads: getPluginDownloads(), projects: getProjects(), average_stars: getAverageStars(), repairitem_download: getDownloadURL(55890), clickspersecond_download: getDownloadURL(57214), securednetwork_download: getDownloadURL(65075)})
+    // Render
+    res.render("home", 
+    {plugin_downloads: data.getPluginDownloads(), 
+        projects: data.getProjects(), 
+        average_stars: data.getAverageStars(), 
+        repairitem: {
+            download: data.getDownloadURL(55890),
+            page: data.getPageURL(55890),
+            docs: "https://davidcubesvk.com/wiki/plugin/RepairItem/"
+        }, 
+        clickspersecond: {
+            download: data.getDownloadURL(57214),
+            page: data.getPageURL(57214),
+            docs: "https://davidcubesvk.com/wiki/plugin/ClicksPerSecond/"
+        },
+        securednetwork: {
+            download: data.getDownloadURL(65075),
+            page: data.getPageURL(65075),
+            docs: "https://davidcubesvk.com/wiki/plugin/SecuredNetwork/"
+        }
+    })
 })
 
+// Create the server
 app.listen(port, () => {
     console.log(`Listening on http://localhost:${port}`)
 })
