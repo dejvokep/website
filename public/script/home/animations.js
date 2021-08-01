e004877b29854bf75be77a8faf260964a9705d51
davidcubesvk <davidcubesvkdev@gmail.com>
2021-08-01 21:12:41 +0200

    New page design.

--- public/script/home/animations.js
+++ public/script/home/animations.js
@@ -10,10 +10,29 @@ for (let i = 0; i < fillValues.length; i++) {
 }
 
 const elements2 = document.getElementsByClassName("value");
-console.log(elements2);
 const toElements = document.getElementsByClassName("to-value");
 for (let i = 0; i < elements2.length; i++) {
     addElement(elements2[i], function () {
         animate(elements2[i], 0, parseInt(toElements[i].innerText)+1, 1000);
     });
+}
+
+const menu = document.getElementById("menu");
+const backToTop = document.getElementById("back-to-top");
+const windowHeight = window.innerHeight;
+const windowWidth = "200px";
+let scrollFunction = window.onscroll;
+window.onscroll = function() {
+    if (scrollFunction != undefined) {
+        scrollFunction();
+    }
+    
+    let scrollTop = document.body.scrollTop;
+    if (scrollTop < windowHeight/3 && menu.classList.contains("scroll-menu")) {
+        menu.classList.remove("scroll-menu");
+        backToTop.style.transform = "translateX(" + windowWidth + ")";
+    } else if (scrollTop >= windowHeight/3 && !menu.classList.contains("scroll-menu")) {
+        menu.classList.add("scroll-menu");
+        backToTop.style.transform = null;
+    }
 }
--- public/script/home/scroll-reveal.js
+++ public/script/home/scroll-reveal.js
@@ -4,7 +4,11 @@ export function addElement(element, callback) {
     elements.set(element, callback);
 }
 
+let scrollFunction = window.onscroll;
 window.onscroll = function() {
+    if (scrollFunction != undefined) {
+        scrollFunction();
+    }
     elements.forEach(function(callback, element) {
         if (element.offsetTop <= document.body.scrollTop + window.innerHeight) {
             callback();
